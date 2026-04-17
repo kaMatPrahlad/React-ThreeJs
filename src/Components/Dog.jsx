bb180c2804ba2f43990902e0a84f3390bc9c47ee
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-04-17 20:46:24 +0530

    Refactor code structure for improved readability and maintainability

--- src/Components/Dog.jsx
+++ src/Components/Dog.jsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
 
 const Dog = () => {
   const model = useGLTF("models/dog.drc.glb");
 
   useThree(({ camera, scene, gl }) => {
     console.log(camera.position);
-    camera.position.z = 1;
+    camera.position.z = 0.55;
+  });
+
+  const textures = useTexture({
+    normalMap: "/dog_normals.jpg",
+  });
+
+  model.scene.traverse((child) => {
+    if (child.name.includes("DOG")) {
+      child.material = new THREE.MeshMatcapMaterial({
+        normalMap: textures.normalMap,
+        color: 0xff0000,
+      });
+    }
   });
 
   return (
     <>
-      <primitive object={model.scene} position={[0, -0.5, 0]} />
+      <primitive
+        object={model.scene}
+        position={[0.25, -0.55, 0]}
+        rotation={[0, Math.PI / 3.5, 0]}
+      />
       <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
       <OrbitControls />
     </>
