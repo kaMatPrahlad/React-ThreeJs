23726079b6b4b628156bf1ddd478a282909b925c
kaMatPrahlad <kamatprahlad43@gmail.com>
2026-04-23 15:51:32 +0530

    background added

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,27 @@ import { Canvas } from "@react-three/fiber";
 function App() {
   return (
     <>
-      <Canvas>
-        <Dog />
-      </Canvas>
+      <main>
+        <Canvas
+          style={{
+            height: "100vh",
+            width: "100vw",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            zIndex: 1,
+            backgroundImage: "url(/background-l.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <Dog />
+        </Canvas>
+
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
     </>
   );
 }
